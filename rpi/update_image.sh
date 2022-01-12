d5e661f1e5a7f54b8e5c9e80277b8d9785f9b5ea
Vincent Bareiß <bareissvincent@gmail.com>
2022-01-12 20:19:18 +0100

    Add git options

--- rpi/update_image.sh
+++ rpi/update_image.sh
@@ -4,12 +4,16 @@ set -e
 SCRIPT_PATH=$(dirname $(realpath "${BASH_SOURCE[0]}"))
 pushd ${SCRIPT_PATH}
 
-#Predefined settings
+#======================= Predefined settings =====================
 CONFIG_FILE="riasc.edgeflex.yaml"
 SSL_SEARCH_PATH="./ssl"
 ASK_CONFIRM=true
 
-#Get User input
+GIT_SERVER="git@git.rwth-aachen.de"
+GIT_ANSIBLE_REPO=${GIT_SERVER}":acs/public/software/pmu/pmu-ansible.git"
+GIT_PASS=${GIT_SERVER}":acs/public/software/pmu/pmu_pass.git"
+
+#========================= Get User input =========================
 usage(){
     echo "Usage:"
     echo "  -I  [Path to Image:                 -I /path/to/image/]"
@@ -18,19 +22,25 @@ usage(){
     echo "  -B  [Git branch                     -B development]"
     echo "  -S  [Path to SSL Cert:              -S /path/to/cert]"
     echo "  -y  [Dont ask for confirmations]"
+    echo ""
+    echo "Credentials for ansible/pass repo"
+    echo "  -U  [${GIT_SERVER} username         -B myName]"
+    echo "  -P  [${GIT_SERVER} pass/token       -P Token]"
     exit
 }
 
 
-while getopts ":I:N:T:B:S::y" opt
+while getopts ":I:N:T:B:S:yU:P:" opt
 do
     case "${opt}" in
         I) IMAGE_FILE=${OPTARG};;
         S) SSL_CERT_FILE=${OPTARG} ;;
         N) NODENAME=${OPTARG} ;;
         y) ASK_CONFIRM=false ;;
-        T) GIT_TOKEN=${OPTARG} ;;
-        B) GIT_BRANCH=${OPTARG} ;;
+        T) PMU_GIT_TOKEN=${OPTARG} ;;
+        B) PMU_GIT_BRANCH=${OPTARG} ;;
+        U) GIT_USERNAME=${OPTARG}
+        P) GIT_PASS=${OPTARG}
         *) echo "Unknown argument ${OPTARG}"
            usage ;;
         :) usage ;;
@@ -48,29 +58,17 @@ if ! [[ -r ${IMAGE_FILE} ]]; then #TODO: Check if this is a .zip file
     usage
 fi
 
-#Ensure nodename is supplied
+#Ensure Nodename is supplied
 if ! [[ -n ${NODENAME} ]]; then
     echo "No node name supplied"
     usage
 fi
 
-#if ! [[ -n ${GIT_TOKEN} ]]; then
-#    echo "No git acces token supplied"
-#    usage
-#fi
-
-
-#Try to find ssl cert file automatically if not supplied
-if ! [[ -n ${SSL_CERT_FILE} ]]; then
-    if [[ -r "${SSL_SEARCH_PATH}/gate-TCP4-1184-${NODENAME}.pmu.acs-lab.eonerc.rwth-aachen.de-config.ovpn" ]]; then
-        SSL_CERT_FILE="${SSL_SEARCH_PATH}/gate-TCP4-1184-${NODENAME}.pmu.acs-lab.eonerc.rwth-aachen.de-config.ovpn"
-        echo "Automatically found SSL cert file: ${SSL_CERT_FILE}"
-    else
-        echo "No SSL cert file supplied. Could not find one either."
-        usage
-    fi
+#Ensure git project token is supplied
+if ! [[ -n ${PMU_GIT_TOKEN} ]]; then
+    echo "No git acces token supplied"
+    usage
 fi
-
 
 #Ensure SSL cert file is found
 if ! [[ -r ${SSL_CERT_FILE} ]]; then
@@ -87,16 +85,21 @@ else
     exit
 fi
 
-#Confirm settings
+exit
+#========================= Check if we can access Git repos =========================
+#Dont do for now
 
+#Check if repos (pass + ansible) are available
+
+#================================= Confirm settings =================================
 echo "Gathered following configuration:"
 echo "Nodename:     ${NODENAME}"
 echo "Image:        ${IMAGE_FILE}"
 echo "SSL_CERT:     ${SSL_CERT_FILE}"
 echo "Config:       ${CONFIG_PATH}"
 
-if [[ -n ${GIT_BRANCH} ]]; then
-echo "Branch:       ${GIT_BRANCH}"
+if [[ -n ${PMU_GIT_BRANCH} ]]; then
+echo "Branch:       ${PMU_GIT_BRANCH}"
 fi
 
 if [[ ${ASK_CONFIRM} == true ]]; then
@@ -108,7 +111,9 @@ if [[ ${ASK_CONFIRM} == true ]]; then
 fi
 
 
-#Start patching:
+#============================== Setup to create patch ==============================
+
+#1. Create temp directory
 echo "Creating temporary work directory"
 if [[ -d ${NODENAME} ]]; then
     echo "Allready exists. Deleting"
@@ -116,7 +121,7 @@ if [[ -d ${NODENAME} ]]; then
 fi
 mkdir ${NODENAME}
 
-#copy what we need
+#2. Copy files to work directory
 echo "Copying files"
 NODE_IMAGE_FILE="${NODENAME}_IMAGE"
 cp ${IMAGE_FILE} "${NODENAME}/${NODE_IMAGE_FILE}.img"
@@ -125,24 +130,34 @@ cp ${CONFIG_PATH} "${NODENAME}/riasc.yaml"
 cp "user-data" "${NODENAME}/user-data"
 echo "Done"
 
+#3. Make sure repos are here
+
+
+#4. Enter working directory
 pushd ${NODENAME}
 
+#5. Generate secrets and write to files
 echo "Generating secrets"
-#Ansible vault key (Currently not used but its there)
+
+#Vault Key
 VAULT_KEY=$(openssl rand -hex 128)
 cat <<EOF > vaultkey.secret
 #!/bin/bash
 echo "${VAULT_KEY}"
 EOF
 
+#Git token
+echo ${PMU_GIT_TOKEN} > "git_token.secret" #TODO: braucht man das??
 
-#GPG key pair
-#TODO
-
-echo ${GIT_TOKEN} > "git_token.secret"
+#SNMP key
+SNMP_KEY=$(openssl rand -hex 10)
+echo ${SNMP_KEY} > "snmp.secret" #TODO: braucht man das??
 
 echo "Done"
 
+#============================ Edit Files for Boot partition ============================
+
+#1. Edit configuration files
 echo "Writing configuration"
 
 sed -i \
@@ -150,28 +165,42 @@ sed -i \
 	riasc.yaml
 
 #Select branch
-if [[ -n ${GIT_BRANCH} ]]; then
+if [[ -n ${PMU_GIT_BRANCH} ]]; then
     sed -i \
-    -e "/url: /a\\\\tbranch: ${GIT_BRANCH}" riasc.yaml
+    -e "/url: /a\\\\tbranch: ${PMU_GIT_BRANCH}" riasc.yaml
 fi
 
 #Git token
-if [[ -n ${GIT_TOKEN} ]]; then
-    sed -i -e "s/git.rwth-aachen/pmu-acs:${GIT_TOKEN}@git.rwth-aachen/g" riasc.yaml
+if [[ -n ${PMU_GIT_TOKEN} ]]; then
+    sed -i -e "s/git.rwth-aachen/pmu-acs:${PMU_GIT_TOKEN}@git.rwth-aachen/g" riasc.yaml
 fi
 
+#Node Name
 sed -i \
 	-e "s/exampleHost/${NODENAME}/g" \
 	user-data
 
 echo "Done"
 
-#Unzip the image file
-#echo "Unzipping the image"
-#unzip -p ${NODE_IMAGE_FILE}.zip > ${NODE_IMAGE_FILE}.img
-#echo "Done"
+#============================== Edit Files in git repo =============================
 
+#1. Encrypt with ansible
+#OpenVPN.
+#SNMP Key
+
+#2. Write variables to ansible-repo
+
+#3. Commit and push ansible-repo
+
+#4. Encrypt password with PGP key
+
+#5. Push to pass repo
+
+#================================== Write to Image ==================================
+
+#1. Write patch file
 echo "Writing Patch file"
+
 cat <<EOF > edgeflex.fish
 echo "Loading image..."
 add ${NODE_IMAGE_FILE}.img
@@ -199,17 +228,19 @@ mkdir /boot/openvpn/
 copy-in acs-lab.conf /boot/openvpn
 EOF
 
-#Write patch to image
+#2. Write patch to image
 echo "Patching image with guestfish..."
 guestfish < edgeflex.fish
 
-# Zip image
+#3. Zip image
+
 #echo "Zipping image..."
 #rm -f ${NODE_IMAGE_FILE}.zip
 #zip ${NODE_IMAGE_FILE}.zip ${NODE_IMAGE_FILE}.img
 #rm -f ${NODE_IMAGE_FILE}.img
 #echo "Done"
 
+#4. Final outputs
 echo "Please write the new image to an SD card:"
 lsblk |grep sd
 echo "  dd bs=1M if=${NODE_IMAGE_FILE}.img of=/dev/sdX"
